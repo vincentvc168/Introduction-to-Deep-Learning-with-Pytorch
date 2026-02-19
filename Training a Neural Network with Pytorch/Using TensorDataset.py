a4db3f688447cafb650488e8fb7d2233bbf0cc6a
Angry Coder <61619999+vincentvc168@users.noreply.github.com>
2026-02-19 19:35:45 +0800

    Implement TensorDataset creation for neural network
    Added functionality to create a TensorDataset from input features and target values.

--- Training a Neural Network with Pytorch/Using TensorDataset.py
+++ Training a Neural Network with Pytorch/Using TensorDataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 animals = pd.read_csv('animal_dataset.csv')
 
+---
 import numpy as np
 
 # Define input features
@@ -13,3 +14,18 @@ print(X)
 target = animals.iloc[:, -1]
 y = target.to_numpy()
 print(y)
+
+---
+import torch
+from torch.utils.data import TensorDataset
+
+X = animals.iloc[:, 1:-1].to_numpy()  
+y = animals.iloc[:, -1].to_numpy()
+
+# Create a dataset
+dataset = TensorDataset(torch.tensor(X), torch.tensor(y))
+
+# Print the first sample
+input_sample, label_sample = dataset[0]
+print('Input sample:', input_sample)
+print('Label sample:', label_sample)
